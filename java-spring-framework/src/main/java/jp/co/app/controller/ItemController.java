0ba95533e7164f79c8b3ca9e9a029e3bd9632a52
r_fujisaki <ryusukefujisaki@gmail.com>
2023-06-05 19:34:02 +0900

    Add delete item action

--- java-spring-framework/src/main/java/jp/co/app/controller/ItemController.java
+++ java-spring-framework/src/main/java/jp/co/app/controller/ItemController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import jp.co.app.form.ItemForm;
@@ -44,6 +46,16 @@ public class ItemController {
 
         itemRepository.store(itemForm.getName());
         redirectAttributes.addFlashAttribute("message", "Item was successfully created.");
+        return "redirect:/items";
+    }
+
+    @DeleteMapping("/items/{id}")
+    public String delete(
+        @PathVariable("id") Integer id,
+        RedirectAttributes redirectAttributes
+    ) {
+        itemRepository.delete(id);
+        redirectAttributes.addFlashAttribute("message", "Item was successfully deleted.");
         return "redirect:/items";
     }
 }
